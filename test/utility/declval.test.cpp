19abe766613023565158cbe3dfc3819d6cfe6142
WentsingNee <1205422966@qq.com>
2021-12-21 14:37:40 +0800

    commit files to main

---
+++ test/utility/declval.test.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file       declval.test.cpp
+ * @brief
+ * @date       2021-12-21
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/utility/declval.hpp>
+
+#include <kerbal/test/test.hpp>
+#include <kerbal/compatibility/method_overload_tag.hpp>
+
+
+typedef char (*CHAR1) [1];
+typedef char (*CHAR2) [2];
+typedef char (*CHAR3) [3];
+typedef char (*CHAR4) [4];
+
+struct Foo
+{
+		CHAR1 f() KERBAL_REFERENCE_OVERLOAD_TAG;
+		CHAR2 f() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
+
+#if __cplusplus >= 201103L
+		CHAR3 f() &&;
+		CHAR4 f() const &&;
+#endif
+
+};
+
+namespace ku = kerbal::utility;
+
+KERBAL_TEST_CASE(test_declval, "test declval")
+{
+	KERBAL_TEST_CHECK_STATIC(sizeof(*(ku::declval<Foo&>().f())) == sizeof(char[1]));
+	KERBAL_TEST_CHECK_STATIC(sizeof(*(ku::declval<const Foo&>().f())) == sizeof(char[2]));
+}
+
+#if __cplusplus >= 201103L
+
+KERBAL_TEST_CASE(test_declval_rvalue, "test declval rvalue")
+{
+	KERBAL_TEST_CHECK_STATIC(sizeof(*(ku::declval<Foo&&>().f())) == sizeof(char[3]));
+	KERBAL_TEST_CHECK_STATIC(sizeof(*(ku::declval<const Foo&&>().f())) == sizeof(char[4]));
+}
+
+#endif
+
+int main(int argc, char * argv[])
+{
+	kerbal::test::run_all_test_case(argc, argv);
+}
