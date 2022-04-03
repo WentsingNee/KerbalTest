6f3978824857c09dbaf1d9488d031e1a7b0ea8f8
WentsingNee <1205422966@qq.com>
2022-04-03 19:44:02 +0800

    commit files to main.fwd guard

--- test/memory/guard.test.cpp
+++ test/memory/guard.test.cpp
@@ -22,42 +22,47 @@ struct A : kerbal::test::object_count<A>
 KERBAL_TEST_CASE(test_guard, "test guard<Tp>")
 {
 	using kerbal::memory::guard;
-	
+
+	std::ptrdiff_t pre = A::get_count();
 	{
 		A * p = new A();
 		guard<A> gd(p);
-		KERBAL_TEST_CHECK(A::get_count() == 1);
+		KERBAL_TEST_CHECK(A::get_count() - pre == 1);
 	}
-	KERBAL_TEST_CHECK(A::get_count() == 0);
+	KERBAL_TEST_CHECK(A::get_count() - pre == 0);
 }
 
 KERBAL_TEST_CASE(test_guard_work_on_array, "test guard<Tp[]>")
 {
 	using kerbal::memory::guard;
-	
+
+	std::ptrdiff_t pre = A::get_count();
 	{
 		int n = 5;
 		A * p = new A[n];
 		guard<A[]> gd(p);
-		KERBAL_TEST_CHECK(A::get_count() == n);
+		KERBAL_TEST_CHECK(A::get_count() - pre == n);
 	}
-	KERBAL_TEST_CHECK(A::get_count() == 0);
+	KERBAL_TEST_CHECK(A::get_count() - pre == 0);
 }
 
 KERBAL_TEST_CASE(test_guard_release, "test guard::release")
 {
 	using kerbal::memory::guard;
-	
-	A * p = new A();
-	guard<A> gd(p);
-	
-	KERBAL_TEST_CHECK(A::get_count() == 1);
-	gd.release();
 
-	KERBAL_TEST_CHECK(A::get_count() == 1);
-	delete p;
+	std::ptrdiff_t pre = A::get_count();
+	{
+		guard<A> gd(new A());
+		KERBAL_TEST_CHECK(A::get_count() - pre == 1);
 
-	KERBAL_TEST_CHECK(A::get_count() == 0);
+		A * p = gd.release();
+		KERBAL_TEST_CHECK(A::get_count() - pre == 1);
+
+		delete p;
+		KERBAL_TEST_CHECK(A::get_count() - pre == 0);
+	}
+
+	KERBAL_TEST_CHECK(A::get_count() - pre == 0);
 }
 
 int main(int argc, char* args[])
