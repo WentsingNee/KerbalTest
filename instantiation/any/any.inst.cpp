fa649c3ac6b671bdde01d4c4170c82a9c3392c7d
WentsingNee <1205422966@qq.com>
2022-06-24 19:28:39 +0800

    any.inst

--- instantiation/any/any.inst.cpp
+++ instantiation/any/any.inst.cpp
@@ -17,12 +17,12 @@ namespace ka = kerbal::any;
 
 typedef kerbal::container::array<int, 10> arr_i_10;
 
-template class ka::basic_any<std::allocator<char> >;
-template ka::basic_any<std::allocator<char> >::basic_any(kerbal::utility::in_place_type_t<int>, const int &);
-template ka::basic_any<std::allocator<char> >::basic_any(kerbal::utility::in_place_type_t<arr_i_10>);
+template class ka::basic_any<>;
+template ka::basic_any<>::basic_any(kerbal::utility::in_place_type_t<int>, const int &);
+template ka::basic_any<>::basic_any(kerbal::utility::in_place_type_t<arr_i_10>);
 
-template void ka::basic_any<std::allocator<char> >::emplace<int>(const int &);
-template void ka::basic_any<std::allocator<char> >::emplace<arr_i_10>();
+template void ka::basic_any<>::emplace<int>(const int &);
+template void ka::basic_any<>::emplace<arr_i_10>();
 
 #if KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_GNU
 #	include <kerbal/config/compiler_private.hpp>
@@ -32,17 +32,17 @@ template void ka::basic_any<std::allocator<char> >::emplace<arr_i_10>();
 #endif
 
 #ifndef KERBAL_TEST_ANY_MEM_REF_QUALIFIER_GXX_5_ICE
-template int ka::basic_any<std::allocator<char> >::get<int>() KERBAL_REFERENCE_OVERLOAD_TAG;
-template int & ka::basic_any<std::allocator<char> >::get<int &>() KERBAL_REFERENCE_OVERLOAD_TAG;
+template int ka::basic_any<>::get<int>() KERBAL_REFERENCE_OVERLOAD_TAG;
+template int & ka::basic_any<>::get<int &>() KERBAL_REFERENCE_OVERLOAD_TAG;
 #endif
-template int ka::basic_any<std::allocator<char> >::get<int>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
-template const int & ka::basic_any<std::allocator<char> >::get<const int &>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
+template int ka::basic_any<>::get<int>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
+template const int & ka::basic_any<>::get<const int &>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
 
 #ifndef KERBAL_TEST_ANY_MEM_REF_QUALIFIER_GXX_5_ICE
-template arr_i_10 ka::basic_any<std::allocator<char> >::get<arr_i_10>() KERBAL_REFERENCE_OVERLOAD_TAG;
-template arr_i_10 & ka::basic_any<std::allocator<char> >::get<arr_i_10 &>() KERBAL_REFERENCE_OVERLOAD_TAG;
+template arr_i_10 ka::basic_any<>::get<arr_i_10>() KERBAL_REFERENCE_OVERLOAD_TAG;
+template arr_i_10 & ka::basic_any<>::get<arr_i_10 &>() KERBAL_REFERENCE_OVERLOAD_TAG;
 #endif
-template arr_i_10 ka::basic_any<std::allocator<char> >::get<arr_i_10>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
-template const arr_i_10 & ka::basic_any<std::allocator<char> >::get<const arr_i_10 &>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
+template arr_i_10 ka::basic_any<>::get<arr_i_10>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
+template const arr_i_10 & ka::basic_any<>::get<const arr_i_10 &>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
 
 
