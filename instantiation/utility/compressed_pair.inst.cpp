d83991f8e35329aebac273767d7119ae96af4e13
WentsingNee <1205422966@qq.com>
2022-10-20 21:30:37 +0800

    修正了 compressed_pair 实例化用例在 vs2017 下 ICE 的问题

--- instantiation/utility/compressed_pair.inst.cpp
+++ instantiation/utility/compressed_pair.inst.cpp
@@ -9,6 +9,7 @@
  *   all rights reserved
  */
 
+#include "msvc_tmpinst_constexpr.hpp"
 #include <kerbal/utility/compressed_pair.hpp>
 
 struct Empty1 {};
@@ -17,8 +18,14 @@ struct Empty2 {};
 namespace ku = kerbal::utility;
 
 template class ku::compressed_pair<int, int>;
-template ku::compressed_pair<int, int>::compressed_pair(const int &, const float &);
-template ku::compressed_pair<int, int>::compressed_pair(const ku::compressed_pair<int &, const int &> &);
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR
+ku::compressed_pair<int, int>::compressed_pair(const int &, const float &) KERBAL_NOEXCEPT;
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR
+ku::compressed_pair<int, int>::compressed_pair(const ku::compressed_pair<int &, const int &> &) KERBAL_NOEXCEPT;
+
+
 
 template class ku::compressed_pair<int, Empty1>;
 template class ku::compressed_pair<Empty1, int>;
@@ -26,7 +33,11 @@ template class ku::compressed_pair<Empty1, Empty1>;
 template class ku::compressed_pair<Empty1, Empty2>;
 
 template class ku::compressed_pair<int[2], int>;
-template ku::compressed_pair<int[2], int>::compressed_pair(const short (&) [2], const float &);
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR
+ku::compressed_pair<int[2], int>::compressed_pair(const short (&) [2], const float &) KERBAL_NOEXCEPT;
+
+
 
 template class ku::compressed_pair<int[2][2], int>;
 template class ku::compressed_pair<int[2][2], int[2][2]>;
