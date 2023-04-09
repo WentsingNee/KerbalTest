113e10e0f139e72a0dfc5a8a3e037015a80edaf6
WentsingNee <1205422966@qq.com>
2023-04-09 22:15:54 +0800

    binary_search instantiation 增加 transparent 查找测试

--- instantiation/algorithm/binary_search.inst.cpp
+++ instantiation/algorithm/binary_search.inst.cpp
@@ -22,6 +22,7 @@ struct cmp_fnptr
 #include <kerbal/container/single_list.hpp>
 
 #include <deque>
+#include <string>
 
 
 template <typename T> struct CtgIter { typedef T* type; };
@@ -37,9 +38,18 @@ template BdrIter<int>::type kerbal::algorithm::lower_bound(BdrIter<int>::type, B
 template FwlIter<int>::type kerbal::algorithm::lower_bound(FwlIter<int>::type, FwlIter<int>::type, const int&, cmp_fnptr<int>::type);
 template FwdIter<int>::type kerbal::algorithm::lower_bound(FwdIter<int>::type, FwdIter<int>::type, const int&, cmp_fnptr<int>::type);
 
+
+template CtgIter<std::string>::type kerbal::algorithm::lower_bound(CtgIter<std::string>::type, CtgIter<std::string>::type, const char * const &);
+template RasIter<std::string>::type kerbal::algorithm::lower_bound(RasIter<std::string>::type, RasIter<std::string>::type, const char * const &);
+template BdrIter<std::string>::type kerbal::algorithm::lower_bound(BdrIter<std::string>::type, BdrIter<std::string>::type, const char * const &);
+template FwlIter<std::string>::type kerbal::algorithm::lower_bound(FwlIter<std::string>::type, FwlIter<std::string>::type, const char * const &);
+template FwdIter<std::string>::type kerbal::algorithm::lower_bound(FwdIter<std::string>::type, FwdIter<std::string>::type, const char * const &);
+
+
 template CtgIter<int>::type kerbal::algorithm::lower_bound_backward(CtgIter<int>::type, CtgIter<int>::type, const int&, cmp_fnptr<int>::type);
 template RasIter<int>::type kerbal::algorithm::lower_bound_backward(RasIter<int>::type, RasIter<int>::type, const int&, cmp_fnptr<int>::type);
 template BdrIter<int>::type kerbal::algorithm::lower_bound_backward(BdrIter<int>::type, BdrIter<int>::type, const int&, cmp_fnptr<int>::type);
+
 
 template CtgIter<int>::type kerbal::algorithm::upper_bound(CtgIter<int>::type, CtgIter<int>::type, const int&, cmp_fnptr<int>::type);
 template RasIter<int>::type kerbal::algorithm::upper_bound(RasIter<int>::type, RasIter<int>::type, const int&, cmp_fnptr<int>::type);
@@ -47,9 +57,18 @@ template BdrIter<int>::type kerbal::algorithm::upper_bound(BdrIter<int>::type, B
 template FwlIter<int>::type kerbal::algorithm::upper_bound(FwlIter<int>::type, FwlIter<int>::type, const int&, cmp_fnptr<int>::type);
 template FwdIter<int>::type kerbal::algorithm::upper_bound(FwdIter<int>::type, FwdIter<int>::type, const int&, cmp_fnptr<int>::type);
 
+
+template CtgIter<std::string>::type kerbal::algorithm::upper_bound(CtgIter<std::string>::type, CtgIter<std::string>::type, const char * const &);
+template RasIter<std::string>::type kerbal::algorithm::upper_bound(RasIter<std::string>::type, RasIter<std::string>::type, const char * const &);
+template BdrIter<std::string>::type kerbal::algorithm::upper_bound(BdrIter<std::string>::type, BdrIter<std::string>::type, const char * const &);
+template FwlIter<std::string>::type kerbal::algorithm::upper_bound(FwlIter<std::string>::type, FwlIter<std::string>::type, const char * const &);
+template FwdIter<std::string>::type kerbal::algorithm::upper_bound(FwdIter<std::string>::type, FwdIter<std::string>::type, const char * const &);
+
+
 template CtgIter<int>::type kerbal::algorithm::upper_bound_backward(CtgIter<int>::type, CtgIter<int>::type, const int&, cmp_fnptr<int>::type);
 template RasIter<int>::type kerbal::algorithm::upper_bound_backward(RasIter<int>::type, RasIter<int>::type, const int&, cmp_fnptr<int>::type);
 template BdrIter<int>::type kerbal::algorithm::upper_bound_backward(BdrIter<int>::type, BdrIter<int>::type, const int&, cmp_fnptr<int>::type);
+
 
 template std::pair<CtgIter<int>::type, CtgIter<int>::type> kerbal::algorithm::equal_range(CtgIter<int>::type, CtgIter<int>::type, int const&, cmp_fnptr<int>::type);
 template std::pair<RasIter<int>::type, RasIter<int>::type> kerbal::algorithm::equal_range(RasIter<int>::type, RasIter<int>::type, int const&, cmp_fnptr<int>::type);
@@ -57,11 +76,29 @@ template std::pair<BdrIter<int>::type, BdrIter<int>::type> kerbal::algorithm::eq
 template std::pair<FwlIter<int>::type, FwlIter<int>::type> kerbal::algorithm::equal_range(FwlIter<int>::type, FwlIter<int>::type, int const&, cmp_fnptr<int>::type);
 template std::pair<FwdIter<int>::type, FwdIter<int>::type> kerbal::algorithm::equal_range(FwdIter<int>::type, FwdIter<int>::type, int const&, cmp_fnptr<int>::type);
 
+
+template std::pair<CtgIter<std::string>::type, CtgIter<std::string>::type>
+kerbal::algorithm::equal_range(CtgIter<std::string>::type, CtgIter<std::string>::type, const char * const &);
+
+template std::pair<RasIter<std::string>::type, RasIter<std::string>::type>
+kerbal::algorithm::equal_range(RasIter<std::string>::type, RasIter<std::string>::type, const char * const &);
+
+template std::pair<BdrIter<std::string>::type, BdrIter<std::string>::type>
+kerbal::algorithm::equal_range(BdrIter<std::string>::type, BdrIter<std::string>::type, const char * const &);
+
+template std::pair<FwlIter<std::string>::type, FwlIter<std::string>::type>
+kerbal::algorithm::equal_range(FwlIter<std::string>::type, FwlIter<std::string>::type, const char * const &);
+
+template std::pair<FwdIter<std::string>::type, FwdIter<std::string>::type>
+kerbal::algorithm::equal_range(FwdIter<std::string>::type, FwdIter<std::string>::type, const char * const &);
+
+
 template CtgIter<int>::type kerbal::algorithm::lower_bound_hint(CtgIter<int>::type, CtgIter<int>::type, const int&, CtgIter<int>::type, cmp_fnptr<int>::type);
 template RasIter<int>::type kerbal::algorithm::lower_bound_hint(RasIter<int>::type, RasIter<int>::type, const int&, RasIter<int>::type, cmp_fnptr<int>::type);
 template BdrIter<int>::type kerbal::algorithm::lower_bound_hint(BdrIter<int>::type, BdrIter<int>::type, const int&, BdrIter<int>::type, cmp_fnptr<int>::type);
 template FwlIter<int>::type kerbal::algorithm::lower_bound_hint(FwlIter<int>::type, FwlIter<int>::type, const int&, FwlIter<int>::type, cmp_fnptr<int>::type);
 template FwdIter<int>::type kerbal::algorithm::lower_bound_hint(FwdIter<int>::type, FwdIter<int>::type, const int&, FwdIter<int>::type, cmp_fnptr<int>::type);
+
 
 template CtgIter<int>::type kerbal::algorithm::upper_bound_hint(CtgIter<int>::type, CtgIter<int>::type, const int&, CtgIter<int>::type, cmp_fnptr<int>::type);
 template RasIter<int>::type kerbal::algorithm::upper_bound_hint(RasIter<int>::type, RasIter<int>::type, const int&, RasIter<int>::type, cmp_fnptr<int>::type);
@@ -69,11 +106,20 @@ template BdrIter<int>::type kerbal::algorithm::upper_bound_hint(BdrIter<int>::ty
 template FwlIter<int>::type kerbal::algorithm::upper_bound_hint(FwlIter<int>::type, FwlIter<int>::type, const int&, FwlIter<int>::type, cmp_fnptr<int>::type);
 template FwdIter<int>::type kerbal::algorithm::upper_bound_hint(FwdIter<int>::type, FwdIter<int>::type, const int&, FwdIter<int>::type, cmp_fnptr<int>::type);
 
+
 template bool kerbal::algorithm::binary_search(CtgIter<int>::type, CtgIter<int>::type, const int&, cmp_fnptr<int>::type);
 template bool kerbal::algorithm::binary_search(RasIter<int>::type, RasIter<int>::type, const int&, cmp_fnptr<int>::type);
 template bool kerbal::algorithm::binary_search(BdrIter<int>::type, BdrIter<int>::type, const int&, cmp_fnptr<int>::type);
 template bool kerbal::algorithm::binary_search(FwlIter<int>::type, FwlIter<int>::type, const int&, cmp_fnptr<int>::type);
 template bool kerbal::algorithm::binary_search(FwdIter<int>::type, FwdIter<int>::type, const int&, cmp_fnptr<int>::type);
+
+
+template bool kerbal::algorithm::binary_search(CtgIter<std::string>::type, CtgIter<std::string>::type, const char * const &);
+template bool kerbal::algorithm::binary_search(RasIter<std::string>::type, RasIter<std::string>::type, const char * const &);
+template bool kerbal::algorithm::binary_search(BdrIter<std::string>::type, BdrIter<std::string>::type, const char * const &);
+template bool kerbal::algorithm::binary_search(FwlIter<std::string>::type, FwlIter<std::string>::type, const char * const &);
+template bool kerbal::algorithm::binary_search(FwdIter<std::string>::type, FwdIter<std::string>::type, const char * const &);
+
 
 template bool kerbal::algorithm::binary_search_hint(CtgIter<int>::type, CtgIter<int>::type, const int&, CtgIter<int>::type, cmp_fnptr<int>::type);
 template bool kerbal::algorithm::binary_search_hint(RasIter<int>::type, RasIter<int>::type, const int&, RasIter<int>::type, cmp_fnptr<int>::type);
