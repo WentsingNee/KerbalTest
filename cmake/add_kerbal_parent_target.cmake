2819e021c8d13453571ca71323b65a3dc602c124
WentsingNee <1205422966@qq.com>
2023-04-21 00:08:35 +0800

    CMake 为每个目标添加父目标

---
+++ cmake/add_kerbal_parent_target.cmake
@@ -0,0 +1,25 @@
+#
+# @file       add_kerbal_parent_target.cmake
+# @brief
+# @date       2023-04-21
+# @author     Peter
+# @copyright
+#      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+#   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+#   all rights reserved
+#
+
+
+function(add_kerbal_parent_target target)
+    string(FIND "${target}" "." pos REVERSE)
+    if (pos EQUAL -1)
+        return ()
+    endif ()
+    string(SUBSTRING "${target}" 0 ${pos} parent_target_name)
+    message(STATUS "${parent_target_name} -> ${target}")
+    if (NOT TARGET ${parent_target_name})
+        add_custom_target(${parent_target_name})
+        add_kerbal_parent_target(${parent_target_name})
+    endif ()
+    add_dependencies(${parent_target_name} ${target})
+endfunction()
