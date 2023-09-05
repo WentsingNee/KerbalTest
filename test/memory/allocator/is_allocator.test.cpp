147211af4980cbd1bb153694736f2cfbd8cbfaef
WentsingNee <1205422966@qq.com>
2023-09-05 16:28:22 +0800

    新增 is_allocator

---
+++ test/memory/allocator/is_allocator.test.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file       is_allocator.test.cpp
+ * @brief
+ * @date       2023-08-12
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/test/test.hpp>
+
+#include <kerbal/memory/allocator/is_allocator.hpp>
+
+
+KERBAL_TEST_CASE(test_is_allocator, "test is_allocator")
+{
+	KERBAL_TEST_CHECK_STATIC(kerbal::memory::is_allocator<std::allocator<int> >::value);
+	KERBAL_TEST_CHECK_STATIC(!kerbal::memory::is_allocator<int>::value);
+}
+
+int main(int argc, char * argv[])
+{
+	kerbal::test::run_all_test_case(argc, argv);
+}
