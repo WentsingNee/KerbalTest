c5ff6792035d32089b359f83dd6ae369ac5b4670
WentsingNee <1205422966@qq.com>
2023-12-04 23:40:17 +0800

    修正了 inst.any.any 中的一处宏定义条件错误

--- instantiation/any/any.inst.cpp
+++ instantiation/any/any.inst.cpp
@@ -32,19 +32,19 @@ void ka::basic_any<>::emplace<arr_i_10>();
 
 #if KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_GNU
 #	include <kerbal/config/compiler_private.hpp>
-#	if KERBAL_GNU_VERSION_MEETS(5, 0, 0)
-#		define KERBAL_TEST_ANY_MEM_REF_QUALIFIER_GXX_5_ICE
+#	if !KERBAL_GNU_VERSION_MEETS(5, 0, 0)
+#		define KTEST_ANY_REF_QUALIFIER_MEMFUN_ICE 1
 #	endif
 #endif
 
-#ifndef KERBAL_TEST_ANY_MEM_REF_QUALIFIER_GXX_5_ICE
+#ifndef KTEST_ANY_REF_QUALIFIER_MEMFUN_ICE
 template int ka::basic_any<>::get<int>() KERBAL_REFERENCE_OVERLOAD_TAG;
 template int & ka::basic_any<>::get<int &>() KERBAL_REFERENCE_OVERLOAD_TAG;
 #endif
 template int ka::basic_any<>::get<int>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
 template const int & ka::basic_any<>::get<const int &>() KERBAL_CONST_REFERENCE_OVERLOAD_TAG;
 
-#ifndef KERBAL_TEST_ANY_MEM_REF_QUALIFIER_GXX_5_ICE
+#ifndef KTEST_ANY_REF_QUALIFIER_MEMFUN_ICE
 template arr_i_10 ka::basic_any<>::get<arr_i_10>() KERBAL_REFERENCE_OVERLOAD_TAG;
 template arr_i_10 & ka::basic_any<>::get<arr_i_10 &>() KERBAL_REFERENCE_OVERLOAD_TAG;
 #endif
