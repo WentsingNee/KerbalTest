9ac7b37869d76c950566b24762b02872d9e8320b
WentsingNee <1205422966@qq.com>
2024-01-18 02:13:05 +0800

    [new-inst][function] 新增 function 的实例化测试

---
+++ instantiation/function/function.inst.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file       function.inst.cpp
+ * @brief
+ * @date       2023-12-30
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#if __cplusplus >= 201103L
+
+#include <ktest/compatibility/msvc_tmpinst_constexpr.hpp>
+#include <kerbal/function/function.hpp>
+
+namespace kf = kerbal::function;
+
+
+namespace
+{
+
+	struct MyLess
+	{
+		bool operator()(int x, int y) const
+		{
+			return x < y;
+		}
+	};
+
+}
+
+
+template
+class kf::basic_function<int()>;
+
+
+
+template
+class kf::basic_function<bool(int, int)>;
+
+template
+kf::basic_function<bool(int, int)>::basic_function(bool(*)(int, int));
+
+template
+kf::basic_function<bool(int, int)>::basic_function(MyLess const &);
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+kf::basic_function<bool(int, int)> &
+kf::basic_function<bool(int, int)>::operator=(bool(* const &)(int, int));
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+kf::basic_function<bool(int, int)> &
+kf::basic_function<bool(int, int)>::operator=(MyLess const &);
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+void
+kf::basic_function<bool(int, int)>::assign(bool(* const &)(int, int));
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+void
+kf::basic_function<bool(int, int)>::assign(MyLess const &);
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+void
+kf::basic_function<bool(int, int)>::emplace<bool(*)(int, int)>(bool(* const &)(int, int));
+
+template KERBAL_MSVC_TMPINST_CONSTEXPR20
+bool
+kf::basic_function<bool(int, int)>::operator()(int const &, int const &) const;
+
+#endif
