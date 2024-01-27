2ce0d2c481bdd1d84e21e84d5ab0adcd2d48ead2
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-01-28 00:48:27 +0800

    [memory] 重构 raw_storage.test.cpp

--- test/memory/raw_storage.test.cpp
+++ test/memory/raw_storage.test.cpp
@@ -9,41 +9,68 @@
  *   all rights reserved
  */
 
+#include <ktest/test/try_test_check.hpp>
+
 #include <kerbal/memory/raw_storage.hpp>
 
 #include <kerbal/test/test.hpp>
 
-#include <iostream>
+#include <kerbal/type_traits/is_copy_assignable.hpp>
+#include <kerbal/type_traits/is_trivially_destructible.hpp>
 
-#if __cplus_plus >= 201103L
-#	include <type_traits>
-#endif
 
-struct Foo
+namespace
 {
-};
 
-struct Goo
-{
-		Goo();
-};
-
-#if __cplus_plus >= 201103L
-
-KERBAL_TEST_CASE(test_raw_storage, "test rawst")
-{
+	struct Foo
 	{
-		typedef kerbal::memory::raw_storage<Foo> rawst;
-		std::cout << std::is_trivially_default_constructible<rawst>::value << std::endl;
-	}
+	};
+
+	struct Goo
 	{
-		typedef kerbal::memory::raw_storage<Goo> rawst;
-		std::cout << std::is_trivially_default_constructible<rawst>::value << std::endl;
-	}
+			~Goo();
+	};
 
 }
 
-#endif
+
+#if __cplusplus >= 201103L
+
+KERBAL_TEST_CASE(test_raw_storage_non_copy_assignable, "test rawst non copy_assignable")
+{
+	using namespace kerbal::type_traits;
+
+#define TRY_TEST_CHECK_WEAK_(Ans, Type) TRY_TEST_CHECK_WEAK(Ans, kerbal::type_traits::try_test_is_copy_assignable<kerbal::memory::raw_storage<Type> >::value)
+
+	TRY_TEST_CHECK_WEAK_(tribool_false, int);
+	TRY_TEST_CHECK_WEAK_(tribool_false, int[4]);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Foo);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Foo[4]);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Goo);
+
+#undef TRY_TEST_CHECK_WEAK_
+
+}
+
+
+KERBAL_TEST_CASE(test_raw_storage_trivially_destructible, "test rawst trivially_destructible")
+{
+	using namespace kerbal::type_traits;
+
+#define TRY_TEST_CHECK_WEAK_(Ans, Type) TRY_TEST_CHECK_WEAK(Ans, kerbal::type_traits::try_test_is_trivially_destructible<kerbal::memory::raw_storage<Type> >::value)
+
+	TRY_TEST_CHECK_WEAK_(tribool_true, int);
+	TRY_TEST_CHECK_WEAK_(tribool_true, int[4]);
+	TRY_TEST_CHECK_WEAK_(tribool_true, Foo);
+	TRY_TEST_CHECK_WEAK_(tribool_true, Foo[4]);
+	// TRY_TEST_CHECK_WEAK_(tribool_true, Goo);
+
+#undef TRY_TEST_CHECK_WEAK_
+
+}
+
+#endif // if __cplusplus >= 201103L
+
 
 int main(int argc, char * argv[])
 {
