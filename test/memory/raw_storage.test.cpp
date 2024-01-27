5b05da8a20fb56ca9796f7cc7f8e8d6f1d9d3618
WentsingNee <1205422966@qq.com>
2024-01-28 00:48:27 +0800

    raw_storage.test.cpp

--- test/memory/raw_storage.test.cpp
+++ test/memory/raw_storage.test.cpp
@@ -16,6 +16,7 @@
 #include <kerbal/test/test.hpp>
 
 #include <kerbal/type_traits/is_copy_assignable.hpp>
+#include <kerbal/type_traits/is_move_assignable.hpp>
 #include <kerbal/type_traits/is_trivially_destructible.hpp>
 
 
@@ -41,6 +42,23 @@ KERBAL_TEST_CASE(test_raw_storage_non_copy_assignable, "test rawst non copy_assi
 	using namespace kerbal::type_traits;
 
 #define TRY_TEST_CHECK_WEAK_(Ans, Type) TRY_TEST_CHECK_WEAK(Ans, kerbal::type_traits::try_test_is_copy_assignable<kerbal::memory::raw_storage<Type> >::value)
+
+	TRY_TEST_CHECK_WEAK_(tribool_false, int);
+	TRY_TEST_CHECK_WEAK_(tribool_false, int[4]);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Foo);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Foo[4]);
+	TRY_TEST_CHECK_WEAK_(tribool_false, Goo);
+
+#undef TRY_TEST_CHECK_WEAK_
+
+}
+
+
+KERBAL_TEST_CASE(test_raw_storage_non_move_assignable, "test rawst non move_assignable")
+{
+	using namespace kerbal::type_traits;
+
+#define TRY_TEST_CHECK_WEAK_(Ans, Type) TRY_TEST_CHECK_WEAK(Ans, kerbal::type_traits::try_test_is_move_assignable<kerbal::memory::raw_storage<Type> >::value)
 
 	TRY_TEST_CHECK_WEAK_(tribool_false, int);
 	TRY_TEST_CHECK_WEAK_(tribool_false, int[4]);
