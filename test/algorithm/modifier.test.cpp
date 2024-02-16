65a72c80e4579710903c8f1449e6c3d7b98c6bfe
WentsingNee <1205422966@qq.com>
2024-02-17 02:58:58 +0800

    拆分 modifier.test.cpp

--- test/algorithm/modifier.test.cpp
+++ test/algorithm/modifier.test.cpp
@@ -111,48 +111,6 @@ KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse_copy, "test reverse_copy(Bid + 0)", 
 
 
 
-template <typename Container, int remain>
-KERBAL_TEMPLATE_TEST_CASE(test_reverse, "test reverse")
-{
-	typedef kerbal::type_traits::integral_constant<std::size_t, 8 * 12 + remain> N;
-
-	int a[N::value]; {
-		kerbal::algorithm::iota(
-				kerbal::container::begin(a), kerbal::container::end(a), 0);
-	}
-
-	Container c(kerbal::container::cbegin(a), kerbal::container::cend(a));
-	kerbal::algorithm::reverse(c.begin(), c.end());
-
-	KERBAL_TEST_CHECK(
-			kerbal::compare::sequence_equal_to(
-					c.rbegin(), c.rend(),
-					kerbal::container::cbegin(a), kerbal::container::cend(a)
-	));
-}
-
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 0)", kerbal::container::vector<int>, 0);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 1)", kerbal::container::vector<int>, 1);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 2)", kerbal::container::vector<int>, 2);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 3)", kerbal::container::vector<int>, 3);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 4)", kerbal::container::vector<int>, 4);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 5)", kerbal::container::vector<int>, 5);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 6)", kerbal::container::vector<int>, 6);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ctg + 7)", kerbal::container::vector<int>, 7);
-
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 0)", std::deque<int>, 0);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 1)", std::deque<int>, 1);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 2)", std::deque<int>, 2);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 3)", std::deque<int>, 3);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 4)", std::deque<int>, 4);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 5)", std::deque<int>, 5);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 6)", std::deque<int>, 6);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Ras + 7)", std::deque<int>, 7);
-
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Bid + 0)", kerbal::container::list<int>, 0);
-KERBAL_TEMPLATE_TEST_CASE_INST(test_reverse, "test reverse(Bid + 1)", kerbal::container::list<int>, 1);
-
-
 template <typename Container>
 KERBAL_TEMPLATE_TEST_CASE(test_rotate, "test rotate")
 {
