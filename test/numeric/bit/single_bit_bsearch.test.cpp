ecbb90ffd89205c3a18ff02a975c2b7007af8429
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-04-14 15:01:06 +0800

    [sync-kerbal][new-facility] 新增 numeric::single_bit_bsearch 的测试

---
+++ test/numeric/bit/single_bit_bsearch.test.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file       single_bit_bsearch.test.cpp
+ * @brief
+ * @date       2024-04-14
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/numeric/bit/single_bit_bsearch.hpp>
+
+#include <kerbal/test/test.hpp>
+#include <kerbal/numeric/numeric_limits.hpp>
+
+
+template <typename T>
+KERBAL_TEMPLATE_TEST_CASE(test_single_bit_bsearch, "test single_bit_bsearch")
+{
+	typedef typename kerbal::numeric::numeric_limits<T>::DIGITS DIGITS;
+	for (int i = 0; i < DIGITS::value; ++i) {
+		T x = static_cast<T>(static_cast<T>(1u) << i);
+		KERBAL_TEST_CHECK_EQUAL(kerbal::numeric::single_bit_bsearch(x), i);
+	}
+}
+
+KERBAL_TEMPLATE_TEST_CASE_INST(test_single_bit_bsearch, "test single_bit_bsearch (unsigned char)", unsigned char);
+KERBAL_TEMPLATE_TEST_CASE_INST(test_single_bit_bsearch, "test single_bit_bsearch (unsigned short)", unsigned short);
+KERBAL_TEMPLATE_TEST_CASE_INST(test_single_bit_bsearch, "test single_bit_bsearch (unsigned int)", unsigned int);
+KERBAL_TEMPLATE_TEST_CASE_INST(test_single_bit_bsearch, "test single_bit_bsearch (unsigned long)", unsigned long);
+KERBAL_TEMPLATE_TEST_CASE_INST(test_single_bit_bsearch, "test single_bit_bsearch (unsigned long long)", unsigned long long);
+
+
+int main(int argc, char * argv[])
+{
+	kerbal::test::run_all_test_case(argc, argv);
+}
