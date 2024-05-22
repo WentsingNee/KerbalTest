3963319f56920d666f8c8c25086af2eb66567e16
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-05-22 16:25:24 +0800

    [new-utest] config 新增对基础类型大小的检测

---
+++ test/config/sizeof_types.test.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file       sizeof_types.test.cpp
+ * @brief
+ * @date       2024-05-10
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/test/test.hpp>
+
+#include <cstdio>
+
+
+KERBAL_TEST_CASE(test_sizeof_integers, "test sizeof(integers)")
+{
+	printf("sizeof(char)      is: %zu\n", sizeof(char));
+	printf("sizeof(short)     is: %zu\n", sizeof(short));
+	printf("sizeof(int)       is: %zu\n", sizeof(int));
+	printf("sizeof(long)      is: %zu\n", sizeof(long));
+	printf("sizeof(long long) is: %zu\n", sizeof(long long));
+}
+
+
+KERBAL_TEST_CASE(test_sizeof_void_p, "test sizeof(void *)")
+{
+	printf("sizeof(void *) is: %zu\n", sizeof(void *));
+}
+
+
+int main(int argc, char * argv[])
+{
+	kerbal::test::run_all_test_case(argc, argv);
+}
