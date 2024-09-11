5fed0dcad2dd76fae7306072d2e3c62d9124afcb
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-09-11 18:00:57 +0800

    新增 rbset

--- test/container/rbtree.test.cpp
+++ test/container/rbtree.test.cpp
@@ -11,55 +11,60 @@
 
 
 #include <iostream>
-#include <kerbal/container/detail/rb_base.hpp>
+#include <kerbal/container/rb_set.hpp>
 
 
 namespace kc = kerbal::container;
 
+template class kc::rb_set<int>;
 
-static void print_rb_normal_result_if_wrong(kc::detail::rb_normal_result_t result)
+namespace
 {
-	switch (result) {
-		case kc::detail::RB_NORMAL_RESULT_CORRECT: {
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_BAD_NODES_CNT: {
-			std::cout << "BAD NODES CNT" << std::endl;
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_BST_INVALID: {
-			std::cout << "BST INVALID" << std::endl;
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_BAD_PARENT: {
-			std::cout << "BAD PARENT" << std::endl;
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_ROOT_NOT_BLACK: {
-			std::cout << "ROOT NOT BLACK" << std::endl;
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_NEIBOUR_RED: {
-			std::cout << "NEIBOUR RED" << std::endl;
-			break;
-		}
-		case kc::detail::RB_NORMAL_RESULT_BAD_BLACK_CNT_IN_PATH: {
-			std::cout << "BAD BLACK CNT IN PATH" << std::endl;
-			break;
+
+	void print_rb_normal_result_if_wrong(kc::detail::rb_normal_result_t result)
+	{
+		switch (result) {
+			case kc::detail::RB_NORMAL_RESULT_CORRECT: {
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_BAD_NODES_CNT: {
+				std::cout << "BAD NODES CNT" << std::endl;
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_BST_INVALID: {
+				std::cout << "BST INVALID" << std::endl;
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_BAD_PARENT: {
+				std::cout << "BAD PARENT" << std::endl;
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_ROOT_NOT_BLACK: {
+				std::cout << "ROOT NOT BLACK" << std::endl;
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_NEIBOUR_RED: {
+				std::cout << "NEIBOUR RED" << std::endl;
+				break;
+			}
+			case kc::detail::RB_NORMAL_RESULT_BAD_BLACK_CNT_IN_PATH: {
+				std::cout << "BAD BLACK CNT IN PATH" << std::endl;
+				break;
+			}
 		}
 	}
-}
 
 
-const char * color_str(kerbal::container::detail::rb_color_t color)
-{
-	if (color == kerbal::container::detail::BLACK::value) {
-		return "BLACK";
-	} else {
-		return "RED";
+	char const * color_str(kerbal::container::detail::rb_color_t color)
+	{
+		if (color == kerbal::container::detail::BLACK::value) {
+			return "BLACK";
+		} else {
+			return "RED";
+		}
 	}
-}
 
+}
 
 
 #include <ktest/random/random_vector.hpp>
@@ -90,7 +95,7 @@ KERBAL_TEST_CASE(test_rb_emplace, "test rb::emplace")
 		for (std::size_t dcase = 0; dcase < kc::size(data); ++dcase) {
 			const kc::vector<int> & d = data[dcase];
 
-			kc::detail::rb_type_only<int> s;
+			kc::rb_set<int> s;
 
 			for (std::size_t i = 0; i < d.size(); ++i) {
 				s.emplace(d[i]);
@@ -128,19 +133,9 @@ void g()
 	std::size_t size = 4 * 1024 * 1024;
 	// std::size_t size = 4;
 
-	// auto d = ktest::get_iota(size, 0);
-	// auto d = ktest::get_random_vec_i_mod(size, eg, 178057);
-	auto d = ktest::get_random_vec_i(size, eg);
-
-	{
-		kerbal::test::runtime_timer t;
-		kc::detail::rb_type_only<int> s;
-		for (auto const & e : d) {
-			s.emplace(e);
-		}
-		std::cout << "kerbal::rbtree  " << t.count() << std::endl;
-		// std::cout << sizeof(s) << std::endl;
-	}
+//	auto d = ktest::get_iota(size, 0);
+	auto d = ktest::get_random_vec_i_mod(size, eg, 178057);
+//	auto d = ktest::get_random_vec_i(size, eg);
 
 	{
 		kerbal::test::runtime_timer t;
@@ -154,11 +149,36 @@ void g()
 
 	{
 		kerbal::test::runtime_timer t;
+		kc::rb_set<int> s;
+		for (auto const & e : d) {
+			s.emplace(e);
+		}
+		std::cout << "kerbal::rbtree  " << t.count() << std::endl;
+		// std::cout << sizeof(s) << std::endl;
+	}
+
+	{
+		kerbal::test::runtime_timer t;
 		kc::avl_set<int> s;
 		for (auto const & e : d) {
 			s.emplace(e);
 		}
 		std::cout << "kerbal::avl_set  " << t.count() << std::endl;
+		// std::cout << sizeof(s) << std::endl;
+	}
+
+	{
+		kerbal::test::runtime_timer t;
+
+		using namespace boost::container;
+		typedef tree_assoc_options< tree_type<red_black_tree> >::type RBTree;
+		typedef boost::container::set<int, std::less<int>, new_allocator<int>, RBTree> RBSet;
+
+		RBSet s;
+		for (auto const & e : d) {
+			s.emplace(e);
+		}
+		std::cout << "boost::rb_set  " << t.count() << std::endl;
 		// std::cout << sizeof(s) << std::endl;
 	}
 
@@ -182,7 +202,7 @@ void g()
 KERBAL_TEST_CASE(test_rb_erase, "test rb::erase")
 {
 	kerbal::container::vector<int> d = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	kc::detail::rb_type_only<int> s;
+	kc::rb_set<int> s;
 
 	for (std::size_t i = 0; i < d.size(); ++i) {
 		s.emplace(d[i]);
@@ -215,8 +235,8 @@ KERBAL_TEST_CASE(test_rb_erase, "test rb::erase")
 
 int main(int argc, char * argv[])
 {
-	// g();
+	 g();
 	// e();
-	// kerbal::test::run_all_test_case(argc, argv);
-	kerbal::test::run_test_case(1, argc, argv);
+//	 kerbal::test::run_all_test_case(argc, argv);
+//	kerbal::test::run_test_case(1, argc, argv);
 }
