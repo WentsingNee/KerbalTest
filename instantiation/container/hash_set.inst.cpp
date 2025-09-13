9b020df2da3c315f71fdbef35cc0efa23b5a63c9
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2025-09-13 21:46:38 +0800

    hash_set.inst

---
+++ instantiation/container/hash_set.inst.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file       hash_set.inst.cpp
+ * @brief
+ * @date       2025-08-30
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/container/hash_set.hpp>
+
+namespace kc = kerbal::container;
+
+template class kc::hash_set<int>;
